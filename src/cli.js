52e373f07eca489ce902be55f57b247fe50ba821
matteoBuccirossi <matteobuccirossi@gmail.com>
2021-01-10 17:36:32 +0100

    generate command line parsing

--- src/cli.js
+++ src/cli.js
@@ -5,7 +5,7 @@ const path = require('path');
 const chalk = require('chalk');
 
 
-import {createBot} from './main';
+import {createBot} from './addNew';
 
 function parseNewArgs(rawArgs){
     const args = arg(
@@ -31,22 +31,78 @@ function parseNewArgs(rawArgs){
     }
 }
 
-async function promptForMissingArgs(options){
-    let questions = [];
+function parseGenArgs(rawArgs){
+    const args = arg(
+        {
+            '--name': String,
+            '--description': String,
+            '--moderation': Boolean,
+            '-n': '--name',
+            '-d': '--description',
+            '-m': '--moderation'
+        },
+        {
+            argv: rawArgs.slice(2),
+        }
+    );
 
-    if(!options.command){
-        questions.push({
-            type: 'confirm',
-            name: 'command',
-            message: 'Add premade moderation commands to your bot?',
-            default: false
-        });
+    if(args['--name']){
+        return{
+            name: args['--name'],
+            description: args['--description'] || '',
+            moderation: args['--moderation'] || false
+        }
+    }else{
+        throw new Error('Missing name');
     }
+}
 
-    const answers = await inquirer.prompt(questions);
-    return {
-        ...options,
-        command: options.command || answers.command,
+async function promptForMissingArgs(options, operation){
+    
+
+    switch(operation){
+        case 'new':
+            let questions = [];
+            if(!options.command){
+                questions.push({
+                    type: 'confirm',
+                    name: 'command',
+                    message: 'Add premade moderation commands to your bot?',
+                    default: false
+                });
+            }
+        
+            const answers = await inquirer.prompt(questions);
+            return {
+                ...options,
+                command: options.command || answers.command,
+            }
+        case 'g':
+            let questions2 = [];
+            if(!options.description){
+                questions2.push({
+                    type: 'input',
+                    name: 'description',
+                    message: 'Add a command description:',
+                    default: ' '
+                })
+            }
+
+            if(!options.moderation){
+                questions2.push({
+                    type: 'confirm',
+                    name: 'moderation',
+                    message: 'Is this a moderation command?',
+                    default: false
+                })
+            }
+
+            const answers2 = await inquirer.prompt(questions2);
+            return {
+                ...options,
+                description: options.description || answers2.description,
+                moderation: options.moderation || answers2.moderation
+            }
     }
 }
 
@@ -59,8 +115,19 @@ export async function cli(args){
         case 'new':
             try{
                 let options = parseNewArgs(args);
-                options = await promptForMissingArgs(options);
+                options = await promptForMissingArgs(options, 'new');
                 await createBot(options);
+                break;
+            }catch(e){
+                console.log(chalk.red(e));
+                process.exit(1);
+            }
+        case 'generate' || 'g':
+            try{
+                let options = parseGenArgs(args);
+                options= await promptForMissingArgs(options, 'g');
+                console.log(options);
+                break;
             }catch(e){
                 console.log(chalk.red(e));
                 process.exit(1);
